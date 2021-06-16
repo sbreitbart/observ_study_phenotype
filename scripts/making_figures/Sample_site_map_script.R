969f457b55f40dba9ca2e2065342bfafac04e279
Sophie Breitbart <sbreitbart@wesleyan.edu>
2021-06-15 21:09:26 -0400

    Finished meteorology rmd and exported figs/tables, then made new sample site map with symbols corresponding in size w/urbanization score

--- scripts/making_figures/Sample_site_map_script.R
+++ scripts/making_figures/Sample_site_map_script.R
@@ -3,6 +3,10 @@
 # Make sample site map
 #=====================
 
+# load libraries
+library(ggmap)
+library(cowplot)
+library(ggsn)
 
 #---------------------
 # Import data
@@ -12,9 +16,15 @@
 ggmap::register_google(key = "AIzaSyDa0p-isKca8Plhs2zSy5inHG9LQTb0MEs")
 
 sites_all <- read.csv(here("./raw_data/80_Sites.csv"), sep = ",", header = TRUE)
-sites <- sites_all[,2:5]
-sites <- sites[c(1:5,10,14,17,21:93),]
-# sbbox <- make_bbox(lon = sites$lon, lat = sites$lat, f = .1)
+sites <- sites_all[,2:6]
+sites <- sites[c(1:3, 5,10,14,17,21:93),]
+
+# Import urb_index values for each of these rows
+urb_scores <- read.csv(here::here("./Figures_Tables/UrbanizationScore/Urbanization_Scores_Table.csv"),
+                       header=T, na.strings=c("","NA")) %>%
+  select(2,8)
+
+sites <- dplyr::inner_join(urb_scores, sites, by = "Patch_ID") 
 
 sites$Transect <- factor(sites$Transect , levels = c("North", "South", "Rural"))
 
@@ -28,7 +38,8 @@ base_map <- ggmap(get_map(location = mylocation, maptype = 'terrain', source = '
 cols1 <- c("North" = "#FDE725FF", "South" = "#21908CFF", "Rural" = "#660099")
 
 map1 <- base_map +
-  geom_point(data = sites, mapping = aes(x = lon, y = lat, fill=Transect, shape=Transect), color = "black", size=3) +
+  geom_point(data = sites, mapping = aes(x = lon, y = lat, fill=Transect, shape=Transect),
+             color = "black", size=3) +
   labs(x = 'Longitude', y = 'Latitude') +
   scalebar(x.min = -80.19, x.max = -79.25,
            y.min = 43.25, y.max = 43.8,
@@ -54,7 +65,87 @@ map1 <- base_map +
 
 
 north2(map1, x = .59, y = .2, scale = 0.11, symbol = 1)
-dev.copy2pdf(file="~/R_Projects/Chapter1/Figures_Tables/SampleSiteMap_terrain_color.pdf", width = 7, height = 5)
+dev.copy2pdf(file="~/R_Projects/chapter_one/Figures_Tables/SampleSiteMaps/SampleSiteMap_terrain_color.pdf",
+             width = 7, height = 5)
+
+
+# add climate graph insets
+# precip_bar <- system.file("extdata", "logo.png", package = "cowplot")
+# 
+# map1_legendmoved <- base_map +
+#   geom_point(data = sites, mapping = aes(x = lon, y = lat, fill=Transect, shape=Transect), color = "black", size=3) +
+#   labs(x = 'Longitude', y = 'Latitude') +
+#   scalebar(x.min = -80.19, x.max = -79.25,
+#            y.min = 43.25, y.max = 43.8,
+#            dist = 10, dist_unit = "km",
+#            st.bottom = FALSE, st.color = "black",
+#            transform = TRUE, model = 'WGS84',
+#            st.dist=.03) +
+#   theme(legend.position = c(0.8, 0.3),
+#         legend.background = element_rect(fill = "white",colour = "black"),
+#         legend.box.margin = margin(6, 6, 6, 6),
+#         legend.text = element_text(size=12),
+#         legend.title = element_text(size = 14),
+#         axis.text=element_text(size=12),
+#         axis.title=element_text(size=14)) +
+#   scale_fill_manual(values = cols1,
+#                     breaks = c("North", "South", "Rural"),
+#                     labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
+#                     name = "Sample Site Transect") +
+#   scale_shape_manual(values = c(23,24,21),
+#                      breaks = c("North", "South", "Rural"),
+#                      labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
+#                      name = "Sample Site Transect") 
+# 
+# ggdraw(map1) +
+#   draw_image(here::here("./Figures_Tables/Meteorology/Monthly_precip_bargraph.png"),
+#              x = .5, y = 1,
+#              hjust = 1, vjust = 1,
+#              width = 0.25, height = 0.25)
+# north2(map1, x = .59, y = .2, scale = 0.11, symbol = 1)
+
+
+map1_urbscore <- base_map +
+  geom_point(data = sites,
+             mapping = aes(x = lon, y = lat, shape=Transect,
+                           fill=Urb_score),
+             color = "black",
+             size = 3) +
+  labs(x = 'Longitude', y = 'Latitude') +
+  scalebar(x.min = -80.19, x.max = -79.25,
+           y.min = 43.25, y.max = 43.8,
+           dist = 10, dist_unit = "km",
+           st.bottom = FALSE, st.color = "black",
+           transform = TRUE, model = 'WGS84',
+           st.dist=.03) +
+  theme(legend.position = "bottom",
+        legend.background = element_rect(fill = "white"),
+        legend.box.margin = margin(6, 6, 6, 6),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size = 14),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.title.align=0.5,
+        legend.box = "vertical")+
+  scale_shape_manual(values = c(23,24,21),
+                     breaks = c("North", "South", "Rural"),
+                     labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
+                     name = "Sample Site Transect") +
+  scale_fill_gradient(low = "white", high = "dark red",
+                      breaks=c(-3.5, 0, 3.5),
+                      labels=c("Rural",0, "Urban"),
+                      limits=c(-3.6, 3.6),
+                      name = "Urbanization\nScore"
+                      ) 
+
+
+north2(map1_urbscore, x = .6, y = .37, scale = 0.11, symbol = 1)
+
+dev.copy2pdf(file="~/R_Projects/chapter_one/Figures_Tables/SampleSiteMaps/SampleSiteMap_terrain_color_urbscore.pdf",
+             width = 7, height = 7)
+
+
+
 
 
 
@@ -90,7 +181,7 @@ map1_satellite <- get_googlemap(center = c(lon = -79.74256, lat = 43.5421), zoom
                      name = "Sample Site Transect") 
 
 north2(map1_satellite, x = .7, y = .5, scale = 0.11, symbol = 1)
-dev.copy2pdf(file="~/R_Projects/Chapter1/Figures_Tables/SampleSiteMap_satellite_color.pdf", width = 10, height = 7)
+dev.copy2pdf(file="~/R_Projects/Chapter1/Figures_Tables/SampleSiteMaps/SampleSiteMap_satellite_color.pdf", width = 10, height = 7)
 
 ##### moving the scale bar & new colors #####
 # map1_satellite <- get_googlemap(center = c(lon = -79.74256, lat = 43.5421), zoom = 10, maptype = 'satellite') %>% ggmap() +
@@ -117,37 +208,6 @@ dev.copy2pdf(file="~/R_Projects/Chapter1/Figures_Tables/SampleSiteMap_satellite_
 #                       breaks = c("North", "South", "Rural"),
 #                        labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
 #                        name = "Sample Site Transect") 
-
-
-
-
-###### incorrect satellite map code #####
-# map1_satellite <- base_map2 +
-#  geom_point(data = sites, mapping = aes(x = lon, y = lat, fill=Transect, shape=Transect), color = "black", size=3) +
-#  labs(x = 'Longitude', y = 'Latitude') +
-#   scalebar(x.min = -80.19, x.max = -79.25,
-#            y.min = 43.25, y.max = 43.8,
-#            dist = 10, dist_unit = "km",
-#            st.bottom = FALSE, st.color = "black",
-#            transform = TRUE, model = 'WGS84',
-#            st.dist=.03) +
-#   theme(legend.position = c(0.8, 0.3),
-#         legend.background = element_rect(fill = "white",colour = "black"),
-#         legend.box.margin = margin(6, 6, 6, 6),
-#         legend.text = element_text(size=12),
-#         legend.title = element_text(size = 14),
-#         axis.text=element_text(size=12),
-#         axis.title=element_text(size=14)) +
-#   scale_fill_manual(values = cols1,
-#                     breaks = c("North", "South", "Rural"),
-#                       labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
-#                       name = "Sample Site Transect") +
-#   scale_shape_manual(values = c(23,24,21),
-#                      breaks = c("North", "South", "Rural"),
-#                       labels = c("Urban: Non-Corridor", "Urban: Corridor", "Rural"),
-#                       name = "Sample Site Transect") 
-
-
 
 
 #---------------------
